d80ab7cc98f0db7a9a975bf90e2d09775b8fe5fe
Nemanja Andrić <nandric00@gmail.com>
2022-09-28 12:44:53 +0200

    Create model service and service host

---
+++ ModelService/Program.cs
@@ -0,0 +1,49 @@
+﻿using ModelWcfServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelServiceHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            // Step 2: Create a ServiceHost instance.
+            ServiceHost selfHost = new ServiceHost(typeof(ModelService), baseAddress);
+
+            try
+            {
+                // Step 3: Add a service endpoint.
+                selfHost.AddServiceEndpoint(typeof(IModelService), new WSHttpBinding(), "ModelService");
+
+                // Step 4: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5: Start the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                // Close the ServiceHost to stop the service.
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ ModelWcfServiceLibrary/ModelService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ModelWcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ModelService : IModelService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
