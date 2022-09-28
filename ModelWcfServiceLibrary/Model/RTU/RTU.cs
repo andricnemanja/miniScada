c0016361f61dedac0e199aa3b2a90769e0dd35fe
Nemanja Andrić <nandric00@gmail.com>
2022-09-28 14:17:20 +0200

    Create model in model service

---
+++ ModelWcfServiceLibrary/Model/RTU/RTU.cs
@@ -0,0 +1,19 @@
+﻿using ModelWcfServiceLibrary.Model.Signals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelWcfServiceLibrary.Model.RTU
+{
+    public class RTU
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public IEnumerable<AnalogSignal> AnalogSignals { get; set; }
+        public IEnumerable<DiscreteSignal> DiscredSignals{ get; set; }
+    }
+}
---
+++ ModelWcfServiceLibrary/Model/Signals/AnalogSignal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelWcfServiceLibrary.Model.Signals
+{
+    public class AnalogSignal
+    {
+        public int Address { get; set; }
+        public string Name { get; set; }
+    }
+}
