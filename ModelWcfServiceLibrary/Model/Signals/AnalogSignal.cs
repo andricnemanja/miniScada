59cd5ba5efdf4128de249e28eaf828d8c80315f9
Nemanja Andrić <nandric00@gmail.com>
2022-10-04 09:05:33 +0200

    Add id for signals

--- ModelWcfServiceLibrary/Model/Signals/AnalogSignal.cs
+++ ModelWcfServiceLibrary/Model/Signals/AnalogSignal.cs
@@ -8,6 +8,7 @@ namespace ModelWcfServiceLibrary.Model.Signals
 {
     public class AnalogSignal
     {
+        public int ID { get; set; }
         public int Address { get; set; }
         public string Name { get; set; }
     }
--- ModelWcfServiceLibrary/Model/Signals/DiscreteSignal.cs
+++ ModelWcfServiceLibrary/Model/Signals/DiscreteSignal.cs
@@ -8,6 +8,7 @@ namespace ModelWcfServiceLibrary.Model.Signals
 {
     public class DiscreteSignal
     {
+        public int ID { get; set; }
         public int Address { get; set; }
         public string Name { get; set; }
     }
