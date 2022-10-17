6f7ac3f96ebc11482b73524f104f816c47dbb793
Nemanja Andrić <nandric00@gmail.com>
2022-10-18 01:09:55 +0200

    Enable dependency injection for Model Service

--- ModelService/Program.cs
+++ ModelService/Program.cs
@@ -18,44 +18,18 @@ namespace ModelServiceHost
         {
             IContainer container = Bootstrapper.RegisterContainerBuilder().Build();
 
-            Uri baseAddress = new Uri("http://localhost:8000/ModbusConnection/");
-            ServiceHost selfHost = new ServiceHost(typeof(ModelService), baseAddress);
-
-            try
-            {
-
-                selfHost.AddDependencyInjectionBehavior<IModelService>(container);
-                selfHost.AddServiceEndpoint(typeof(IModelService), new BasicHttpBinding(), "ModelService");
-
-                // Step 4: Enable metadata exchange.
-                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-                smb.HttpGetEnabled = true;
-
-
-                IComponentRegistration registration;
-                if (!container.ComponentRegistry.TryGetRegistration(new TypedService(typeof(IModelService)), out registration))
-                {
-                    Console.WriteLine("The service contract has not been registered in the container.");
-                    Console.ReadLine();
-                    Environment.Exit(-1);
-                }
-
-                selfHost.Description.Behaviors.Add(smb);
-                selfHost.Open();
-                Console.WriteLine("The service is ready.");
-
-                Console.WriteLine("Press <Enter> to terminate the service.");
-                Console.WriteLine();
-                Console.ReadLine();
-                selfHost.Close();
-            }
-            catch (CommunicationException ce)
-            {
-                Console.WriteLine("An exception occurred: {0}", ce.Message);
-                selfHost.Abort();
-            }
-
             ServiceHost selfHost = new ServiceHost(typeof(ModelService));
+            selfHost.AddDependencyInjectionBehavior<IModelService>(container);
+
+            IComponentRegistration registration;
+            if (!container.ComponentRegistry.TryGetRegistration(new TypedService(typeof(IModelService)), out registration))
+            {
+                Console.WriteLine("The service contract has not been registered in the container.");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+
+            
             selfHost.Open();
 
             string listenUri = selfHost.Description.Endpoints[0].ListenUri.AbsoluteUri;
