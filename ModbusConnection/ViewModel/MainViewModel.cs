93a2ca49795b5458890b803a9314c1ee5f6efd86
Nemanja Andrić <nandric00@gmail.com>
2022-10-27 09:53:47 +0200

    Read model service static data from client application

--- ModbusConnection/ViewModel/MainViewModel.cs
+++ ModbusConnection/ViewModel/MainViewModel.cs
@@ -13,74 +13,60 @@ using System.Windows.Threading;
 
 namespace ModbusConnection.ViewModel
 {
-    internal class MainViewModel
-    {
+	internal class MainViewModel
+	{
 
-        public ICommand CreateConnectionWindowCommand { get; set; }
-        public ICommand SetRegistryValuesCommand { get; set; }
+		public ICommand CreateConnectionWindowCommand { get; set; }
+		public ICommand SetRegistryValuesCommand { get; set; }
 
-        public ObservableCollection<RTU> RTUList { get; set; }
-
-
-        private ValuesReadingService valuesReadingService;
-
-        public MainViewModel()
-        {
-            CreateConnectionWindowCommand = new CreateConnectionWindowCommand();
-            SetRegistryValuesCommand = new SetRegistryValuesCommand();
-
-            valuesReadingService = new ValuesReadingService();
-
-            ObservableCollection<ISignal> discreteSignals1 = new ObservableCollection<ISignal>()
-            {
-                new DiscreteSignal(1, "Prekidac 1", false),
-                new DiscreteSignal(2, "Prekidac 2", false)
-            };
-            ObservableCollection<ISignal> discreteSignals2 = new ObservableCollection<ISignal>()
-            {
-                new DiscreteSignal(0, "Prekidac 1", false),
-                new DiscreteSignal(2, "Prekidac 2", false),
-                new DiscreteSignal(5, "Prekidac 3", false)
-
-            };
-            ObservableCollection<ISignal> discreteSignals3 = new ObservableCollection<ISignal>()
-            {
-                new DiscreteSignal(1, "Prekidac 1", false)
-            };
-
-            ObservableCollection<ISignal> analogSignals1 = new ObservableCollection<ISignal>()
-            {
-                new AnalogSignal(1, "Analogni signal 1", 0),
-            };
-            ObservableCollection<ISignal> analogSignals2 = new ObservableCollection<ISignal>()
-            {
-                new AnalogSignal(1, "Analogni signal 1", 0),
-                new AnalogSignal(5, "Analogni signal 2", 0)
-            };
-            ObservableCollection<ISignal> analogSignals3 = new ObservableCollection<ISignal>()
-            {
-                new AnalogSignal(1, "Analogni signal 1", 0),
-                new AnalogSignal(2, "Analogni signal 2", 0)
-            };
+		public ObservableCollection<RTU> RTUList { get; set; } = new ObservableCollection<RTU>();
 
 
+		private ValuesReadingService valuesReadingService;
 
-            RTUList = new ObservableCollection<RTU>
-            {
-                new RTU(new RTUData("RTU 1", "127.0.0.1", 502), new RTUValues(discreteSignals1, analogSignals1), new RTUConnection(null)),
-                new RTU(new RTUData("RTU 2", "127.0.0.1", 503), new RTUValues(discreteSignals2, analogSignals2), new RTUConnection(null)),
-                new RTU(new RTUData("RTU 3", "127.0.0.1", 504), new RTUValues(discreteSignals3, analogSignals3), new RTUConnection(null))
-            };
+		public MainViewModel()
+		{
+			CreateConnectionWindowCommand = new CreateConnectionWindowCommand();
+			SetRegistryValuesCommand = new SetRegistryValuesCommand();
 
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
-            timer.Start();
-        }
+			valuesReadingService = new ValuesReadingService();
 
-        void timer_Tick(object sender, EventArgs e)
-        {
-            valuesReadingService.ReadValues(RTUList);
-        }
-    }
+			ConnectToModelService();
+
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += timer_Tick;
+			timer.Start();
+		}
+
+		public void ConnectToModelService()
+		{
+			ModelServiceReference.ModelServiceClient modelService = new();
+
+			foreach(var rtu in modelService.GetStaticDataAsync().Result)
+			{
+				ObservableCollection<ISignal> discreteSignals = new ObservableCollection<ISignal>();
+				ObservableCollection<ISignal> analogSignals = new ObservableCollection<ISignal>();
+
+				foreach (var discreteSignal in rtu.DiscreteSignals)
+				{
+					discreteSignals.Add(new DiscreteSignal(discreteSignal.Address, discreteSignal.Name, false));
+				}
+				foreach (var analogSignal in rtu.AnalogSignals)
+				{
+					analogSignals.Add(new AnalogSignal(analogSignal.Address, analogSignal.Name, 0));
+				}
+
+				RTUList.Add(new RTU(new RTUData(rtu.Name, rtu.Address, rtu.Port), 
+									new RTUValues(discreteSignals, analogSignals),
+									new RTUConnection(null)));
+
+			}
+		}
+
+		void timer_Tick(object sender, EventArgs e)
+		{
+			valuesReadingService.ReadValues(RTUList);
+		}
+	}
 }
