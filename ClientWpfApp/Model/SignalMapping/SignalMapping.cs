351fd50130b54a88144b5c585aaad32438cbb820
Nemanja Andrić <nandric00@gmail.com>
2022-12-28 15:17:41 +0100

    Create signal mapping model class

---
+++ ClientWpfApp/Model/SignalMapping/SignalMapping.cs
@@ -0,0 +1,13 @@
+﻿namespace ClientWpfApp.Model.SignalMapping
+{
+	public class SignalMapping
+	{
+		public int RtuId { get; set; }
+		public int AnalogSignalId { get; set; }
+		public int MinSensorValue { get; set; }
+		public int MaxSensorValue { get; set; }
+		public int MinRealValue { get; set; }
+		public int MaxRealValue { get; set;}
+		public string Unit { get; set; }
+	}
+}
