42e960360c94c4fedcf37a7b8d718cf5fcfbe669
Nemanja Andrić <nandric00@gmail.com>
2022-12-28 15:24:32 +0100

    Create repository for SignalMapping

---
+++ ModbusServiceLibrary/Repository/SignalMappingRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ModbusServiceLibrary.Model.SignalMapping;
+
+namespace ModbusServiceLibrary.Repository
+{
+	public class SignalMappingRepository
+	{
+		public List<SignalMapping> SignalMappings { get; set; }
+	}
+}
