8d23d792747a21229d80a81491275973c3b570a1
Nemanja Andrić <nandric00@gmail.com>
2023-01-12 09:51:05 +0100

    Disable discrete signal command button based on signal state

---
+++ ClientWpfApp/Converters/DiscreteSignalButtonStatusConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ClientWpfApp.Converters
+{
+	internal class DiscreteSignalButtonStatusConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if ((string)values[0] == (string)values[1])
+				return false;
+			return true;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
