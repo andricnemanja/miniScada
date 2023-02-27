946c4825e3baf7a3ac9d51d86565f9b60ca5adca
Nemanja Andrić <nandric00@gmail.com>
2023-02-27 16:21:55 +0100

    Change ConcurentQueue to BlockingCollection for command result processing

--- ClientWpfApp/ModbusCallback/CommandResultQueue.cs
+++ ClientWpfApp/ModbusCallback/CommandResultQueue.cs
@@ -6,38 +6,26 @@ namespace ClientWpfApp.ModbusCallback
 {
 	public class CommandResultQueue
 	{
-		private readonly ConcurrentQueue<CommandResultBase> commandResults = new ConcurrentQueue<CommandResultBase>();
-		private readonly ManualResetEvent queueEvent;
+		private readonly BlockingCollection<CommandResultBase> commandResults = new BlockingCollection<CommandResultBase>();
 		private readonly ICommandResultReceiver commandResultReceiver;
 
 		public CommandResultQueue(ICommandResultReceiver commandResultReceiver)
 		{
 			this.commandResultReceiver = commandResultReceiver;
-			queueEvent = new ManualResetEvent(false);
 			Thread thread = new Thread(ProcessResults);
 			thread.Start();
 		}
 
 		public void AddToQueue(CommandResultBase commandResult)
 		{
-			commandResults.Enqueue(commandResult);
-			queueEvent.Set();
+			commandResults.Add(commandResult);
 		}
 
 		private void ProcessResults()
 		{
 			while (true)
 			{
-				queueEvent.WaitOne();
-
-				if(commandResults.TryDequeue(out CommandResultBase nextCommandResult)) 
-				{
-					commandResultReceiver.ReceiveCommandResult(nextCommandResult);
-				}
-				else
-				{
-					queueEvent.Reset();
-				}
+				commandResultReceiver.ReceiveCommandResult(commandResults.Take());
 			}
 		}
 	}
